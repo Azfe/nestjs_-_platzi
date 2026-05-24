ec368eec5ab5334b5473590b6276544e3eec20fe
Alex Zapata <alexzapata1984@gmail.com>
2026-05-24 19:59:00 +0200

    refactor(posts): split service, controller and DTOs into dedicated subfolders

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Post } from './entities/post.entity';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from '../entities/post.entity';
+import { CreatePostDto } from '../dtos/create-post.dto';
+import { UpdatePostDto } from '../dtos/update-post.dto';
 
 @Injectable()
 export class PostsService {
